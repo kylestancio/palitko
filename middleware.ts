0dc9e27d7fbc622d4174412b233ee46c91096448
kylestancio <kylestancio@gmail.com>
2023-09-25 12:31:03 +0900

    [fix] protect /orders/* and /cart/* pages

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { withAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
@@ -27,4 +26,8 @@ export default withAuth(
   }
 )
 
-export const config = { matcher: ["/manager/:path*"] }
+export const config = { matcher: [
+  "/manager/:path*",
+  "/orders/:path*",
+  "/cart/:path*"
+] }
